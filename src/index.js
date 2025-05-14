70363eb9afc61b428e69df3bc7b9a74d974eb1b8
Kenny5231 <124184507+Kenny5231@users.noreply.github.com>
2025-05-14 14:32:17 -0600

    Incorporacion de nuevas herremientas

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // Asegúrate de que App.jsx exista y esté bien escrito
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
